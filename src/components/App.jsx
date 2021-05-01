e56e363f8a4d6792ff7207226ac837619e6f864c
Denis <DenisOleksiuk95@gmail.com>
2021-05-01 13:38:30 +0300

    init: project structure

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
+
+export const App = () => {
+  return (
+    <div>
+      App
+      <SearchBar />
+      <ImageList />
+    </div>
+  );
+};
---
+++ src/components/ImageList.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ImageList extends Component {
+  render() {
+    return <div>ImageList</div>;
+  }
+}
